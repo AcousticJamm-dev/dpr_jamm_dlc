42c0c82a00fc8fc484b5b1136585a33fc9258d33
AcousticJamm <jasmicheals@gmail.com>
2026-04-19 11:30:01 -0400

    Achievements
    TODO: Images for the achievements

---
+++ achievements.lua
@@ -0,0 +1,51 @@
+local achievements = {
+	{
+		id = "jamm",
+		name = "A New Friend",
+		desc = "Unlock Jamm as a party member.",
+		hint = "Unlock a party member.",
+		border = "party"
+	},
+	
+	{
+		id = "marcy",
+		name = "Unbending Loyalty",
+		desc = "Unlock Marcy as an assist party member for Jamm.",
+		hint = "Unlock an assist party member.",
+		border = "party"
+	},
+	
+	{
+		id = "willy",
+		name = "Harpoon of the Deep",
+		desc = "Unlock Willy as a party member.",
+		hint = "Unlock a party member.",
+		border = "party"
+	},
+	
+	{
+		id = "dungeon",
+		name = "Rememberance",
+		desc = "Complete the Forgotten Wood portion of Jamm's DLC.",
+		hint = "Complete a sidequest.",
+		border = "common"
+	},
+	
+	{
+		id = "pirate",
+		name = "Off the Deep End",
+		desc = "Complete the Emerald Seas portion of Jamm's DLC.",
+		hint = "Complete a sidequest.",
+		border = "common"
+	},
+	
+	{
+		id = "finale",
+		name = "Do you trust me?",
+		desc = "Complete the finale of Jamm's DLC.",
+		hint = "Complete Jamm's DLC.",
+		border = "common"
+	},
+}
+
+return achievements
